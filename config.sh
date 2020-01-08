b0b58175b06833ad98103f8a5bd5c9b44cbb021f
Joseph Curtis <jccurtis@lbl.gov>
2020-01-07 18:49:15 -0800

    Change config cmds to run if associated system files/dirs are present. This should prevent this stages from running during a docker build.

--- config.sh
+++ config.sh
@@ -8,14 +8,18 @@
 # Insert into line starting with GRUB_CMDLINE_LINUX_DEFAULT
 # Don't insert if line contains usbcore.usbfs_memory_mb=1000
 # Insert at the end of the line before the "
-sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ { /usbcore.usbfs_memory_mb=1000/! s|"$| usbcore.usbfs_memory_mb=1000"|g }' /etc/default/grub
-sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ { /noresume/! s|"$| noresume"|g }' /etc/default/grub
-sudo update-grub
+if [[ -f /etc/default/grub ]]; then
+    sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ { /usbcore.usbfs_memory_mb=1000/! s|"$| usbcore.usbfs_memory_mb=1000"|g }' /etc/default/grub
+    sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/ { /noresume/! s|"$| noresume"|g }' /etc/default/grub
+    sudo update-grub
+fi
 
-# NOTE this doesn't work when building in docker. The above grub command should complete this step on every (future) boot.
+# NOTE this doesn't work when building in docker.
 # Increase the max USB image size to 1000MB in USB module
 # https://importgeek.wordpress.com/2017/02/26/increase-usbfs-memory-limit-in-ubuntu/
 # https://support.pixelink.com/support/solutions/articles/3000054087-image-transfer-fails-to-start-when-image-size-is-bigger-than-2-mb
-# sudo sh -c "echo 1000 > /sys/module/usbcore/parameters/usbfs_memory_mb"
+if [[ -d /sys/module/usbcore/parameters ]]; then
+    sudo sh -c "echo 1000 > /sys/module/usbcore/parameters/usbfs_memory_mb"
+fi
 
 # TODO Add UDEV rules?
